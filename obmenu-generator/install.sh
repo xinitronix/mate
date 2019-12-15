4c15b6c53006021dce17f1359e7c0feb666fcc14
xinitronix <55929274+xinitronix@users.noreply.github.com>
2019-12-15 21:40:55 +0300

    Update install.sh

--- obmenu-generator/install.sh
+++ obmenu-generator/install.sh
@@ -9,3 +9,4 @@ chown  -R      $login:wheel        /home/$login/.config/obmenu-generator
 cp             schema.pl           /home/$login/.config/obmenu-generator
 cpanm Linux::DesktopFiles
 cpanm Data::Dump
+sed -i -e  '1 s/^/use XSLoader;\n/' /usr/local/lib/perl5/site_perl/mach/5.30/GDBM_File.pm
