09005f19d255e4b12554328e82993f9b0b51ac7c
xinitronix <55929274+xinitronix@users.noreply.github.com>
2019-12-15 21:15:16 +0300

    Add files via upload

--- obmenu-generator/install.sh
+++ obmenu-generator/install.sh
@@ -3,8 +3,8 @@ login=definitly
 CURRENTDIRECTORY=$(pwd)
 
 
-cp       obmenu-generator /usr/bin/local
+cp       obmenu-generator /usr/local/bin
 mkdir    /home/$login/.config/obmenu-generator
-cp       schema.pl /home/$login/obmenu-generator
+cp       schema.pl /home/$login/.obmenu-generator
 cpanm Linux::DesktopFiles
 cpanm Data::Dump
