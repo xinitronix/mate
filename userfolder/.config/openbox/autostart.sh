86fb161edc928651b28a35d778114d925fe2d054
xinitronix <55929274+xinitronix@users.noreply.github.com>
2020-04-24 18:57:17 +0300

    Update autostart.sh

--- userfolder/.config/openbox/autostart.sh
+++ userfolder/.config/openbox/autostart.sh
@@ -5,7 +5,7 @@ feh --bg-scale ~/.config/openbox/wallpaper/$pic
 setxkbmap -layout us,ru -option "grp:alt_shift_toggle,grp_led:scroll" -variant ",winkeys"  &  
 #xcompmgr -cfCF &
 xset m 0 & 
-sbxkb &
+xxkb &
 firefox &
 ~/scripts/x2.sh &
 
