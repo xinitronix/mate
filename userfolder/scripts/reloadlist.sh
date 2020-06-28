e0f38966f6a50cc4e80902dbd326c03dc3cc9fe9
xinitronix <55929274+xinitronix@users.noreply.github.com>
2020-06-28 16:23:30 +0300

    Add files via upload

--- userfolder/scripts/reloadlist.sh
+++ userfolder/scripts/reloadlist.sh
@@ -3,6 +3,7 @@
 #The Webserver interface must be enabled. You can find it in Settings->Services->Webserver
 #Set a custom port like 6969 (don't use 80 or 8080)
 #Edit the $URL var with your custom port
+sshpass -p  639639 scp list.m3u  pi@192.168.8.45:/var/www/html
 
 JSON='{"jsonrpc":"2.0","method":"Addons.SetAddonEnabled","params":{"addonid":"pvr.iptvsimple","enabled":"toggle"},"id":1}'
 URL="http://192.168.8.45:8081/jsonrpc"
