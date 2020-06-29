e89326fd0da97d10f91b8141fd14370269d26f8c
Your Name <you@example.com>
2020-06-29 10:32:50 +0300

    ----0:wq

--- userfolder/scripts/notify.sh
+++ userfolder/scripts/notify.sh
@@ -2,3 +2,4 @@
 export DISPLAY=:0.0
 export XAUTHORITY=/home/definitly/.Xauthority
 /usr/local/bin/notify-send --urgency normal --expire-time=1000000000 -i typing-monitor -h int:x:1500 -h int:y:1500 "SIM" "Не забыть вынуть симку из роутера"
+
