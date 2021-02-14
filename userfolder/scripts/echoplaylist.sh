916085b0b1ae20f699205739331f755819ff1154
Ваше Имя <you@example.com>
2021-02-15 00:46:58 +0300

    новый файл:    userfolder/scripts/echoplaylist.sh

---
+++ userfolder/scripts/echoplaylist.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+
+ sed '$d' list.m3u >> list.m3u.new
+ mv list.m3u.new list.m3u
+ echo $1 '| user-agent=Mozilla/5.0 (X11; FreeBSD amd64; rv:77.0) Gecko/20100101 Firefox/77.0 ' >> list.m3u
