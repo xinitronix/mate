f33caed2cd8f231ad3497ada0cbd6f79999447e7
xinitronix <55929274+xinitronix@users.noreply.github.com>
2021-08-14 07:19:22 +0300

    Update f.sh

--- userfolder/f.sh
+++ userfolder/f.sh
@@ -2,3 +2,5 @@
 rm -R ~/.mozilla/firefox
 scp -r  pi@192.168.8.45:/home/pi/tinder/3/firefox ~/.mozilla
 scp -r  pi@192.168.8.45:/home/pi/TelegramDesktop ~/.local/share
+fetch https://addons.mozilla.org/firefox/downloads/file/3816867/ublock_origin-1.37.2-an+fx.xpi
+firefox ublock_origin-1.37.2-an+fx.xpi
