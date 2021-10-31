de004536e947c786d0589dc596742ea79bb99c56
xinitronix <55929274+xinitronix@users.noreply.github.com>
2021-10-31 14:20:59 +0300

    Update echoplaylist.sh

--- userfolder/scripts/echoplaylist.sh
+++ userfolder/scripts/echoplaylist.sh
@@ -4,3 +4,4 @@
  sed '$d' list.m3u >> list.m3u.new
  mv list.m3u.new list.m3u
  echo $1'|user-agent=Mozilla/5.0 (X11; FreeBSD amd64; rv:77.0) Gecko/20100101 Firefox/77.0 ' >> list.m3u
+./reloadlist.sh
