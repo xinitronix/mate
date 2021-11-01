23fb3fc4ed8cc5ea32fbc153bba24e791d0f8902
xinitronix <55929274+xinitronix@users.noreply.github.com>
2021-11-01 23:46:36 +0300

    Update f.sh

--- userfolder/f.sh
+++ userfolder/f.sh
@@ -4,3 +4,7 @@ sshpass -p  639639 scp -r  pi@192.168.8.45:/home/pi/tinder/3/firefox ~/.mozilla
 sshpass -p  639639 scp -r  pi@192.168.8.45:/home/pi/TelegramDesktop ~/.local/share
 #fetch https://addons.mozilla.org/firefox/downloads/file/3816867/ublock_origin-1.37.2-an+fx.xpi
 #firefox ublock_origin-1.37.2-an+fx.xpi
+doas zpool import zada2
+doas zpool import media1
+doas zpool import media2
+doas zpool import ntfs-2TB
