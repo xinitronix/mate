1f49d232b2a7d4466447542f744df12ef9c2b32c
xinitronix <55929274+xinitronix@users.noreply.github.com>
2021-11-12 17:08:26 +0300

    Update install.sh

--- xboxdrv/install.sh
+++ xboxdrv/install.sh
@@ -7,5 +7,6 @@ CURRENTDIRECTORY=$(pwd)
                       echo 'xboxdrv_enable="YES"'        >>                   /etc/rc.conf 
                       cp  $CURRENTDIRECTORY/xboxdrv        /usr/local/etc/rc.d
                       chmod 744                            /usr/local/etc/rc.d/xboxdrv
-                  
-        
+                      fetch  https://github.com/xinitronix/bin/raw/master/xboxdrv
+                      chmod +x xboxdrv
+                      cp xboxdrv /usr/local/bin
