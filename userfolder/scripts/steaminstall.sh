e569d91ed5c6988394a7952e14d17d7669921146
Your Name <you@example.com>
2021-11-14 02:36:01 +0300

    изменено:      git.sh 	изменено:      userfolder/scripts/chantimicro.sh 	новый файл:    userfolder/scripts/steaminstall.sh

---
+++ userfolder/scripts/steaminstall.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+steam-install --allow-stealing-my-passwords,-browser-history-and-ssh-keys && steam && /usr/local/wine-proton/bin/pkg32.sh install -y -r myrepo_i386 wine-proton mesa-dri && cd /usr/local/bin && \
+ doas ln -s /usr/local/wine-proton/bin/wine proton && cd 
