f86c5a211aacd73dec3debd8d6a29b4cdc0761a4
xinitronix <55929274+xinitronix@users.noreply.github.com>
2021-12-14 23:11:54 +0300

    Update installallpkg.sh

--- userfolder/scripts/package/installallpkg.sh
+++ userfolder/scripts/package/installallpkg.sh
@@ -4,6 +4,6 @@
    for pkg in $(pkg search -r myrepo -g \* | sed -r 's/([^ ]*).*/\1/'); do
 
      echo $pkg
-     doas pkg  install  -r myrepo  $pkg
+     doas pkg  install  -r myrepo -y $pkg
 
    done
