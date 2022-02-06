3ff0d293b0448b3c6c1e2b1387a1e1e74581a903
Your Name <you@example.com>
2022-02-06 15:28:50 +0300

    новый файл:    userfolder/scripts/package/ftp_upload.sh

---
+++ userfolder/scripts/package/ftp_upload.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+ncftpput  -R -z -r 10 -v -u "pi" -p  "639639"     192.168.8.45             /home/pi/     ~/All
+ncftpput  -z -r 10 -v -u "pi" -p     "639639"     192.168.8.45             /home/pi/All  ~/All/packagesite.txz
+ncftpput  -z -r 10 -v -u "pi" -p     "639639"     192.168.8.45             /home/pi/All  ~/All/meta.txz
+
+
+shpass -p 639639 ssh pi@192.168.8.45 'chmod -R 777 /home/pi/All'
