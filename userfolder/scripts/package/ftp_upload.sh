e4bb6240c13ffb95f02c915738a710f3577b6ceb
xinitronix <55929274+xinitronix@users.noreply.github.com>
2022-02-06 17:42:35 +0300

    Update ftp_upload.sh

--- userfolder/scripts/package/ftp_upload.sh
+++ userfolder/scripts/package/ftp_upload.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-ncftpput  -R -z -r 10 -v -u "pi" -p  "639639"     192.168.8.45             /home/pi/     ~/All
-ncftpput  -z -r 10 -v -u "pi" -p     "639639"     192.168.8.45             /home/pi/All  ~/All/packagesite.txz
-ncftpput  -z -r 10 -v -u "pi" -p     "639639"     192.168.8.45             /home/pi/All  ~/All/meta.txz
+ncftpput  -R -z -r 10 -v -u "pi" -p     "639639"     192.168.8.45             /home/pi/     ~/All
+ncftpput  -z    -r 10 -v -u "pi" -p     "639639"     192.168.8.45             /home/pi/All  ~/All/packagesite.txz
+ncftpput  -z    -r 10 -v -u "pi" -p     "639639"     192.168.8.45             /home/pi/All  ~/All/meta.txz
 
 
 sshpass -p 639639 ssh pi@192.168.8.45 'chmod -R 777 /home/pi/All'
