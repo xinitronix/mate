9113a8a8afd733e03e8fe47a11fab78149e56c54
xinitronix <55929274+xinitronix@users.noreply.github.com>
2022-02-13 19:49:12 +0300

    Update echoplaylist.sh

--- userfolder/scripts/echoplaylist.sh
+++ userfolder/scripts/echoplaylist.sh
@@ -5,3 +5,7 @@
  mv list.m3u.new list.m3u
  echo $1'|user-agent=Mozilla/5.0 (X11; FreeBSD amd64; rv:77.0) Gecko/20100101 Firefox/77.0 ' >> list.m3u
 ./reloadlist.sh
+./reloadlist.sh
+sleep 3
+curl -X POST -H "Content-Type: application/json" -d '{"jsonrpc":"2.0","id":1,"method":"Player.Open","params":{"item":{"channelid":17}}}' http://192.168.8.45:8081/jsonrpc
+
