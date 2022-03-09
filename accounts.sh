5b0e40b6132caceac4f05f0d66f35a7f9ea8bb2e
Your Name <you@example.com>
2022-03-09 20:30:44 +0300

    изменено:      accounts.sh 	изменено:      config.sh 	новый файл:    userfolder/scripts/firefox_vpn.sh

--- accounts.sh
+++ accounts.sh
@@ -7,12 +7,14 @@ passwd1=639639
 login1=ff
 passwd2=639639
 login2=rtorrent
+passwd3=639639
+login3=vpn
 
 #create accounts
               echo $passwd1 | pw useradd $login1 -m -g wheel -s /bin/csh -h 0
               echo $passwd  | pw useradd $login  -m -g wheel -s /bin/csh -h 0
               echo $passwd2 | pw useradd $login2 -m -g wheel -s /bin/csh -h 0
-
+              echo $passwd3 | pw useradd $login3 -m -g wheel -s /bin/csh -h 0
               pw groupmod video    -m $login
               pw groupmod operator -m $login
               pw groupmod vboxusers -m $login
---
+++ userfolder/scripts/firefox_vpn.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+xhost +local:vpn
+doas su -l vpn  -c  firefox
