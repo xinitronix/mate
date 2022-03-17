188782b3585976d546129945d571ec51813bb2bc
Your Name <you@example.com>
2022-03-17 23:12:45 +0300

    новый файл:    userfolder/scripts/download_tor_folder.sh

---
+++ userfolder/scripts/download_tor_folder.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+doas mkdir -p /home/vpn/.mozilla
+doas mkdir -p /home/vpn/.config
+doas chown -R vpn  /home/vpn/.mozilla
+doas chmod -R 0777 /home/vpn/.mozilla
+
+doas chown -R vpn   /home/vpn/.config/
+doas chmod -R 0777  /home/vpn/.config/
+sshpass -p  639639 scp -r  pi@192.168.8.45:/home/pi/firefox_vpn/firefox /home/vpn/.mozilla
+sshpass -p  639639 scp -r  pi@192.168.8.45:/home/pi/chromium /home/vpn/.config
+doas chown -R vpn  /home/vpn/.mozilla
+doas chmod -R 0777 /home/vpn/.mozilla
+
+doas chown -R vpn   /home/vpn/.config/chromium
+doas chmod -R 0777  /home/vpn/.config/chromium
