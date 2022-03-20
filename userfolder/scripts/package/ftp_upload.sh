f72c088cef2612314d030f198b7866ca46c66890
Your Name <you@example.com>
2022-03-20 10:20:30 +0300

    изменено:      userfolder/scripts/package/ftp_upload.sh

--- userfolder/scripts/package/ftp_upload.sh
+++ userfolder/scripts/package/ftp_upload.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-pkg repo ~/All
+pkg repo /ntfs-2TB/All
 
-ncftpput  -R -z -r 10 -v -u "pi" -p     "639639"     192.168.8.45             /home/pi/     ~/All
-ncftpput     -z -r 10 -v -u "pi" -p     "639639"     192.168.8.45             /home/pi/All  ~/All/packagesite.txz
-ncftpput     -z -r 10 -v -u "pi" -p     "639639"     192.168.8.45             /home/pi/All  ~/All/meta.txz
+ncftpput  -R -z -r 10 -v -u "pi" -p     "639639"     192.168.8.45             /home/pi/      /ntfs-2TB/All
+ncftpput     -z -r 10 -v -u "pi" -p     "639639"     192.168.8.45             /home/pi/All   /ntfs-2TB/All/packagesite.txz
+ncftpput     -z -r 10 -v -u "pi" -p     "639639"     192.168.8.45             /home/pi/All   /ntfs-2TB/All/meta.txz
 
 sshpass -p 639639 ssh pi@192.168.8.45 'chmod -R 777 /home/pi/All'
