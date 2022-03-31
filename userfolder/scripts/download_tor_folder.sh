bfaf7b9bfcbf9dd4380907126002d0333d373bec
Your Name <you@example.com>
2022-03-31 23:06:57 +0300

    новый файл:    userfolder/.scripts/iridium_vpn.sh 	изменено:      userfolder/scripts/download_tor_folder.sh

--- userfolder/scripts/download_tor_folder.sh
+++ userfolder/scripts/download_tor_folder.sh
@@ -8,10 +8,13 @@ doas chown -R vpn   /home/vpn/.config/
 doas chmod -R 0777  /home/vpn/.config/
 sshpass -p  639639 scp -r  pi@192.168.8.45:/home/pi/firefox_vpn/firefox /home/vpn/.mozilla
 sshpass -p  639639 scp -r  pi@192.168.8.45:/home/pi/chromium.tar.gz  /home/vpn/.config
+sshpass -p  639639 scp -r  pi@192.168.8.45:/home/pi/iridium.tar.gz  /home/vpn/.config
 doas chown -R vpn  /home/vpn/.mozilla
 doas chmod -R 0777 /home/vpn/.mozilla
 
 doas tar xfvz /home/vpn/.config/chromium.tar.gz -C /home/vpn/.config
-
+doas tar xfvz /home/vpn/.config/iridium.tar.gz -C /home/vpn/.config
 doas chown -R vpn   /home/vpn/.config/chromium
 doas chmod -R 0777  /home/vpn/.config/chromium
+doas chown -R vpn   /home/vpn/.config/iridium
+doas chmod -R 0777  /home/vpn/.config/iridium
