42d37aad175e6b9207a155d031bfeb701036eb80
xinitronix <55929274+xinitronix@users.noreply.github.com>
2022-04-30 22:45:34 +0300

    Update steaminstall.sh

--- userfolder/scripts/steaminstall.sh
+++ userfolder/scripts/steaminstall.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-
+doas pkg install -r myrepo -y linux-steam-utils-20220115 libc6-shim-20211220 python3 linux-nvidia-libs
 steam-install --allow-stealing-my-passwords,-browser-history-and-ssh-keys && steam && /usr/local/wine-proton/bin/pkg32.sh install -y -r myrepo_i386 wine-devel  wine-proton mesa-dri && cd /usr/local/bin && \
  doas ln -s /usr/local/wine-proton/bin/wine proton && cd 
