04a9b0f629f7bc6618bcb242773b434733c896db
xinitronix <55929274+xinitronix@users.noreply.github.com>
2023-11-23 21:41:59 +0300

    Update streamlink_kodi.sh

--- userfolder/scripts/streamlink_kodi.sh
+++ userfolder/scripts/streamlink_kodi.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
-m3u=$(echo  "streamlink $1 720p" | base64 | tr -d '\r\n')
+#m3u=$(echo  "streamlink $1 720p" | base64 | tr -d '\r\n')
 
-
+m3u=$(streamlink --stream-url $1)
 sed '$d' list.m3u >> list.m3u.new
 mv list.m3u.new list.m3u
 rm /tmp/list.m3u
-echo ""http://127.0.0.1:53422/base64/$m3u/"|user-agent=Mozilla/5.0 (X11; FreeBSD amd64; rv:77.0) Gecko/20100101 Firefox/77.0" >> /tmp/list.m3u
+#echo ""http://127.0.0.1:53422/base64/$m3u/"|user-agent=Mozilla/5.0 (X11; FreeBSD amd64; rv:77.0) Gecko/20100101 Firefox/77.0" >> /tmp/list.m3u
+echo "$m3u|user-agent=Mozilla/5.0 (X11; FreeBSD amd64; rv:77.0) Gecko/20100101 Firefox/77.0" >> /tmp/list.m3u
 ./reloadlist
 ./reloadlist
 sleep 3
