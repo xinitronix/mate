b5cafec46cdd4d616657cb91ae647de1710acd30
xinitronix <55929274+xinitronix@users.noreply.github.com>
2024-04-18 11:47:35 +0300

    Update ftp_upload.sh

--- userfolder/scripts/package/ftp_upload.sh
+++ userfolder/scripts/package/ftp_upload.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-pkg repo /ntfs-2TB/All
+doas pkg repo /ntfs-2TB/All
 
 ncftpput  -R -z -r 10 -v -u "pi" -p     "639639"     192.168.8.45             /home/pi/      /ntfs-2TB/All
 ncftpput     -z -r 10 -v -u "pi" -p     "639639"     192.168.8.45             /home/pi/All   /ntfs-2TB/All/packagesite.txz
