3619cb7a592a50a8074c64df3c0e94cd222af86e
xinitronix <55929274+xinitronix@users.noreply.github.com>
2024-05-22 08:07:41 +0300

    Update mountiso.sh

--- userfolder/.scripts/mountiso.sh
+++ userfolder/.scripts/mountiso.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
-if [ "$(id -u)" != "0" ]; then
-   echo "This script must be run as root" 1>&2
-   exit 1
-fi
+#if [ "$(id -u)" != "0" ]; then
+ #  echo "This script must be run as root" 1>&2
+  # exit 1
+#fi
 
-
- mdconfig -a -t vnode -f $1  -u 1
- mount -t cd9660 /dev/md1 /mnt/
+doas mdconfig -a -t vnode -f "$1"  -u 1
+doas mount -t cd9660 /dev/md1 /mnt/
