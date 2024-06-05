1fb31149a53560f11fc860d806e6e46ea6295ad1
Your Name <you@example.com>
2024-06-05 20:34:45 +0300

    новый файл:    dwm/applications/defaults.list 	новый файл:    dwm/applications/mimeapps.list 	новый файл:    dwm/applications/mimeinfo.cache 	новый файл:    dwm/applications/userapp-Firefox-XIRGBX.desktop 	новый файл:    dwm/applications/userapp-burniso.sh-R98AO2.desktop 	новый файл:    dwm/applications/userapp-burniso.sh-VAD8N2.desktop 	новый файл:    dwm/applications/userapp-djview-6J07HX.desktop 	новый файл:    dwm/applications/userapp-djvu-8DCGIX.desktop 	новый файл:    dwm/applications/userapp-feh_browser-03YRII.desktop 	новый файл:    dwm/applications/userapp-leafpad-S85CCX.desktop 	новый файл:    dwm/applications/userapp-mountiso.sh-3KVUO2.desktop 	новый файл:    dwm/applications/userapp-mountiso.sh-IBI1N2.desktop 	новый файл:    dwm/applications/userapp-mplayer-7JZ7FX.desktop 	новый файл:    dwm/applications/userapp-mplayer-APXHAX.desktop 	новый файл:    dwm/applications/userapp-mplayer-C2XB6W.desktop 	новый файл:    dwm/applications/userapp-mplayer-D41HCX.desktop 	новый файл:    dwm/applications/userapp-set_wallpaper-02YRII.desktop 	новый файл:    dwm/applications/userapp-tarxf-ZVS3DX.desktop 	новый файл:    dwm/applications/userapp-umountiso.sh-DXMSN2.desktop 	новый файл:    dwm/applications/userapp-undeb-H0NJBX.desktop 	новый файл:    dwm/applications/userapp-unrpm-ZEPGCX.desktop 	новый файл:    dwm/applications/userapp-wine-PKS79W.desktop 	новый файл:    dwm/applications/userapp-xpdf-01YRII.desktop 	изменено:      dwm/install.sh

--- dwm/install.sh
+++ dwm/install.sh
@@ -2,9 +2,12 @@
  
 login=dwm
 
-    cp config.sh /home/$login
+    cp config.h /home/$login
     cp xinitrc   /home/$login/.xinitrc
     cp Xdefaults /home/$login/.Xdefaults
-
+    cp -r applications  /home/$login/.local/share
     chown  -R      $login:wheel   /home/$login/.xinitrc
     chown  -R      $login:wheel   /home/$login/.Xdefaults
+    chown  -R      $login:wheel   /home/$login/config.h
+    chown  -R      $login:wheel   /home/$login/.local/share/applications
+    su dwm -c 'update-desktop-database /home/dwm/.local/share/applications'
