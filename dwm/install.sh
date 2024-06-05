d154ffe7ff33f7376a15208db4d8de1e7fdb138c
Your Name <you@example.com>
2024-06-05 19:22:45 +0300

    изменено:      dwm/install.sh 	изменено:      dwm/xinitrc

--- dwm/install.sh
+++ dwm/install.sh
@@ -5,3 +5,6 @@ login=dwm
     cp config.sh /home/$login
     cp xinitrc   /home/$login/.xinitrc
     cp Xdefaults /home/$login/.Xdefaults
+
+    chown  -R      $login:wheel   /home/$login/.xinitrc
+    chown  -R      $login:wheel   /home/$login/.Xdefaults
