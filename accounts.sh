4b84dbe7eb8fd343ad024c380ad98ccf3d2bdaf9
Your Name <you@example.com>
2024-06-05 18:06:11 +0300

    изменено:      accounts.sh

--- accounts.sh
+++ accounts.sh
@@ -14,7 +14,7 @@ login3=dwm
               echo $passwd1 | pw useradd $login1 -m -g wheel -s /bin/csh -h 0
               echo $passwd  | pw useradd $login  -m -g wheel -s /bin/csh -h 0
               echo $passwd2 | pw useradd $login2 -m -g wheel -s /bin/csh -h 0
-              echo $passwd3 | pw useradd $login3 -m -g wheel -s /bin/csh -h 0
+              echo $passwd3 | pw useradd $login3 -m -g wheel -s /bin/sh -h 0
               pw groupmod video    -m $login
               pw groupmod operator -m $login
               pw groupmod vboxusers -m $login
