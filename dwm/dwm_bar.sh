ed10c6c541f8bcd258fb5370af24aca3164af5e2
Your Name <you@example.com>
2024-06-10 11:29:11 +0300

    изменено:      dwm/dwm_bar.sh

--- dwm/dwm_bar.sh
+++ dwm/dwm_bar.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
+max_num=100
 
 
 while true; 
 do  
+i=0
+tempcity=$(curl -s "https://api.openweathermap.org/data/2.5/weather?id=524894&appid=9cabf88b4c9c983d726a820860e3a5b6&units=metric" | jq -r '.main.temp')℃
+
+ while   [ $i -le $max_num ] ; do
+ i=`expr $i + 1`
 
 date=$( date +"%F %R" )
-tempcity=$(curl -s "https://api.openweathermap.org/data/2.5/weather?id=524894&appid=9cabf88b4c9c983d726a820860e3a5b6&units=metric" | jq -r '.main.temp')℃
 freemem=$(free | grep 'mem_free:' | awk '{print $5}' | sed 's/.$//') 
 tcpu=tCPU:$(sysctl -n dev.cpu.0.temperature | cut -c 1-2)℃
 topprocess=$(top -bn 1 | awk '{print $12}' | awk '(NR == 10)')
@@ -18,4 +23,7 @@ freessd=SSD:$(df -H / |  awk '{print $4 }'  | awk '(NR == 2)')
       xsetroot -name "$compat $skb $tempcity $freessd $freemem $tcpu $loadcpu $topprocess $loadtop $date"
 
       sleep 5
+done
+
+       sleep 1
 done
