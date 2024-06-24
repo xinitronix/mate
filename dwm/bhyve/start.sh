fbec4cea133117ef7ce66dadae4baa9239443154
Your Name <you@example.com>
2024-06-24 13:00:56 +0300

    изменено:      dwm/bhyve/start.sh

--- dwm/bhyve/start.sh
+++ dwm/bhyve/start.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
- barriers -a 192.168.8.100  --config ~/bhyve/win   --disable-crypto --disable-client-cert-checking
+X :3 &
+export DISPLAY=:3
+exec   setxkbmap -layout us,ru -option "grp:alt_shift_toggle,grp_led:scroll" -variant ",winkeys"  & 
+ barriers -a 192.168.8.100  --config ~/bhyve/win   --disable-crypto --disable-client-cert-checking --display :3
 
 ./detach.sh &
 ./win10.sh
