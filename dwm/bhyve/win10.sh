ef71ad063b882c7c276f57bb687e963fa747eed3
xinitronix <55929274+xinitronix@users.noreply.github.com>
2024-06-27 14:20:48 +0300

    Update win10.sh

--- dwm/bhyve/win10.sh
+++ dwm/bhyve/win10.sh
@@ -12,17 +12,52 @@ IF="tap0"
 MAC="mac=00:A0:98:78:32:10"
 DPY="w=1920,h=1080"
 
-doas  ifconfig $IF up
+
+
+detach() {
+echo stop
+
+ devctl detach  -f pci0:0:20:0
+ devctl set driver   pci0:0:20:0 xhci
+ devctl attach pci0:0:20:0
+ devctl detach -f  pci0:0:2:0
+ devctl set driver pci0:0:2:0 vgapci
+ devctl attach  pci0:0:2:0
+echo stop 2
+}
+
+
+attach() {
+echo start
+ devctl detach pci0:0:20:0
+ devctl set driver pci0:0:20:0 ppt
+ devctl set driver pci0:0:2:0 vgapci
+ devctl detach pci0:0:2:0
+ devctl set driver pci0:0:2:0 ppt
+
+}
+
+
+detach_vgapci &
+  sleep 2
+  detach_xhci &
+  sleep 2
+  detach
+  sleep 2
+  attach
+
+  ifconfig $IF up
 while true
 do
-    doas  bhyve \
+      bhyve \
       -c 4,sockets=2,cores=2 -S -A  \
       -s 0,hostbridge \
       -s 4,ahci-cd,$CD \
       -s 3,ahci-hd,$HD,sectorsize=512 \
       -s 5,fbuf,tcp=0.0.0.0:5900,$DPY \
       -s 6,xhci,tablet \
-      -s 2,passthru,0/2/0,rom=/home/dwm/GM107.rom \
+      -s 2,passthru,0/2/0 \
+      -s 11,passthru,0/20/0 \
       -s 10,virtio-net,$IF \
       -s 20,hda,play=/dev/dsp,rec=/dev/dsp \
       -s 31,lpc \
@@ -31,10 +66,12 @@ do
       $VM
 
     RES=$?
-    doas  bhyvectl --destroy --vm=$VM
+      bhyvectl --destroy --vm=$VM
     if [ $RES -eq 1 ]
     then
-        exit 1
+
+detach
+        break
     fi
     echo sleeping for 5 sec...
     sleep 5
