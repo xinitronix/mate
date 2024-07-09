0cbec6278e45e952aef3a8e16b071b07b907c418
Your Name <you@example.com>
2024-07-09 12:35:47 +0300

    новый файл:    dwm/bhyve/linuxmint.sh 	изменено:      userfolder/scripts/package/install.sh

--- userfolder/scripts/package/install.sh
+++ userfolder/scripts/package/install.sh
@@ -5,7 +5,7 @@ kldload linux64
 
   for i in $(cat pkg_leaves); do
 
-#pkg install  -r myrepo  -yf  $i 
-pkg install  -r FreeBSD  -y $i 
+pkg install  -r myrepo  -yf  $i 
+#pkg install  -r FreeBSD  -y $i 
 
    done
