99bbab2d98867bb054fe461bb9edfe45e8251e5e
Your Name <you@example.com>
2024-09-07 09:51:13 +0300

    изменено:      zapret/install.sh

--- zapret/install.sh
+++ zapret/install.sh
@@ -9,4 +9,4 @@ echo 'firewall_script="/etc/rc.firewall.my"' >> /etc/rc.conf
 echo 'ipfw -q -f flush' >> /etc/rc.firewall.my
 echo 'ipfw add 100 divert 989 tcp from any to any 80,443 out not diverted xmit re0' >> /etc/rc.firewall.my
 echo 'ipfw add 100 divert 989 tcp from any 80,443 to any tcpflags syn,ack in not diverted recv re0' >> /etc/rc.firewall.my
-echo '/usr/local/bin/dvtws --port 989  --daemon  --dpi-desync=fake --dpi-desync-fooling=datanoack --debug 1' >> /etc/rc.firewall.my
+echo '/usr/local/bin/dvtws --port 989  --daemon  --dpi-desync=fake,split2 --dpi-desync-autottl=1:1-10 --dpi-desync-fooling=md5sig --debug 1' >> /etc/rc.firewall.my
