72379f9b7c1168f2aee92b7c9c9e0e2bb2ff4a55
Your Name <you@example.com>
2024-10-01 14:29:41 +0300

    изменено:      userfolder/scripts/ftp_edit_tor.sh

--- userfolder/scripts/ftp_edit_tor.sh
+++ userfolder/scripts/ftp_edit_tor.sh
@@ -2,4 +2,4 @@
 
 sshpass -p "639639" scp pi@192.168.8.45:/etc/tor/torrc /tmp/torrc
 leafpad  /tmp/torrc
-sshpass -p "639639" scp /tmp/torrc pi@192.168.8.45:/etc/tor/torrc
+sshpass -p "639639" scp /tmp/torrc root@192.168.8.45:/etc/tor/torrc
