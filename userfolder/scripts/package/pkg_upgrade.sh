d7f78aa0fc2baa0999b5c147cb3047bb7de08461
Your Name <you@example.com>
2024-10-04 08:41:28 +0300

    изменено:      userfolder/scripts/package/pkg_upgrade.sh

--- userfolder/scripts/package/pkg_upgrade.sh
+++ userfolder/scripts/package/pkg_upgrade.sh
@@ -13,9 +13,20 @@ rm pkg_upgrade
 
 echo N |  pkg upgrade -r FreeBSD | grep - | sed -r 's/([^ ]*).*/\1/' | tr -d ":" >> /tmp/pkg_upgrade.txt
 
+
+
  for i in `cat /tmp/pkg_upgrade.txt` ; do 
     
   rm /ntfs-2TB/All/$i-*
   
 
 done
+
+pkg upgrade  -r  FreeBSD
+
+for i in `cat /tmp/pkg_upgrade.txt` ; do 
+
+         sshpass -p 639639 ssh pi@192.168.8.45 "rm ~/All/$i-*"
+
+
+  done 
