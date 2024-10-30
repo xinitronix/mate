748e20d9c53ebdfeb2b6810a5a368e11dc0da6b2
Your Name <you@example.com>
2024-10-30 19:06:10 +0300

    изменено:      userfolder/scripts/package/ftp_upload.sh

--- userfolder/scripts/package/ftp_upload.sh
+++ userfolder/scripts/package/ftp_upload.sh
@@ -8,12 +8,12 @@ ncftpput     -z -r 10 -v -u "pi" -p     "639639"     192.168.8.45             /h
 
 pkg repo /ntfs-2TB/All_i386
 
-ncftpput  -R -z -r 10 -v -u "pi" -p     "639639"     192.168.8.45             /home/pi/      /ntfs-2TB/All_i386
-ncftpput     -z -r 10 -v -u "pi" -p     "639639"     192.168.8.45             /home/pi/All   /ntfs-2TB/All_i386/packagesite.txz
-ncftpput     -z -r 10 -v -u "pi" -p     "639639"     192.168.8.45             /home/pi/All   /ntfs-2TB/All_i386/meta.txz
+ncftpput  -R -z -r 10 -v -u "pi" -p     "639639"     192.168.8.45             /home/pi/           /ntfs-2TB/All_i386
+ncftpput     -z -r 10 -v -u "pi" -p     "639639"     192.168.8.45             /home/pi/All_i386   /ntfs-2TB/All_i386/packagesite.txz
+ncftpput     -z -r 10 -v -u "pi" -p     "639639"     192.168.8.45             /home/pi/All_i386   /ntfs-2TB/All_i386/meta.txz
 
 
-ncftpput     -z -r 10 -v -u "pi" -p     "639639"     192.168.8.45             /home/pi/All   /ntfs-2TB/All/packagesite.txz
+ncftpput     -z -r 10 -v -u "pi" -p     "639639"     192.168.8.45             /home/pi/All        /ntfs-2TB/All/packagesite.txz
 ncftpput     -z -r 10 -v -u "pi" -p     "639639"     192.168.8.45             /home/pi/All_i386   /ntfs-2TB/All_i386/packagesite.txz
 
 sshpass -p 639639 ssh pi@192.168.8.45 'chmod -R 777 /home/pi/All_i386'
