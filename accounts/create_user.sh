fd471c4be812286a96875c6d0e146a69bb00cd3b
Your Name <you@example.com>
2024-11-27 16:41:32 +0300

    новый файл:    accounts/create_user.sh 	новый файл:    accounts/user 	изменено:      install.sh

---
+++ accounts/create_user.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+cat  user  | while read line
+   do
+      
+login=$(echo $line | awk '{print $1}' )
+passwd=$(echo $line | awk '{print $2}')
+echo $passwd | pw useradd $login -m -g wheel -s /bin/sh -h 0
+              pw groupmod video    -m $login
+              pw groupmod operator -m $login
+              pw groupmod vboxusers -m $login
+
+   done
