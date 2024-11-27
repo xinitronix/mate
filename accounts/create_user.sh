64bea8b59a45eb8e75ae91333b96acf26f539d6f
Your Name <you@example.com>
2024-11-27 17:34:53 +0300

    изменено:      accounts/create_user.sh

--- accounts/create_user.sh
+++ accounts/create_user.sh
@@ -5,7 +5,7 @@ cat  user  | while read line
       
 login=$(echo $line | awk '{print $1}' )
 passwd=$(echo $line | awk '{print $2}')
-echo $passwd | pw useradd $login -m -g wheel -s /bin/sh -h 0
+echo $passwd | pw useradd $login -m -g wheel -s /bin/csh -h 0
               pw groupmod video    -m $login
               pw groupmod operator -m $login
               pw groupmod vboxusers -m $login
