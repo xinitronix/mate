874db9498f7531ef82b0c43de741de47f68ab069
Your Name <you@example.com>
2024-12-05 17:21:24 +0300

    изменено:      deinstall/dump 	изменено:      ubuntu_chroot/install.sh 	изменено:      ubuntu_chroot/ubuntu_chroot 	новый файл:    ubuntu_chroot/ubuntu_chroot_change

--- ubuntu_chroot/install.sh
+++ ubuntu_chroot/install.sh
@@ -3,5 +3,6 @@
 cp ubuntu  /etc/rc.d
 ln -s                     /ntfs-2TB/compat/ubuntu /compat/ubuntu
 cp ubuntu_chroot          /usr/local/bin
+cp ubuntu_chroot_change         /usr/local/bin
 cp ubuntu_chroot.desktop  /usr/local/share/applications
 cp ubuntu_check_status    /usr/local/bin
