4611679414ee15ed68d8065ce670df43a2ca6866
Your Name <you@example.com>
2025-01-27 00:33:05 +0300

    удалено:       userfolder/.scripts/dsd.sh 	удалено:       userfolder/.scripts/killpid.sh 	удалено:       userfolder/.scripts/skype

--- userfolder/.scripts/dsd.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-
-
-killall -9 dsd
-xterm -geometry 110x20+600+650 -e  "padsp -- dsd -i /dev/dsp -o /dev/dsp"
--- userfolder/.scripts/killpid.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
- 
-   lsof -t /compat/ubuntu/dev >> /tmp/killpid.log
-
-   for i in `cat  /tmp/killpid.log` ; do 
-       doas kill -9  $i
-     done
