c7e053b2bc592c436861e032367bffb3372a7180
Your Name <you@example.com>
2025-01-26 21:42:05 +0300

    новый файл:    userfolder/scripts/delete_ntfs-2TB.sh 	удалено:       userfolder/scripts/tcpdump_flv.sh 	удалено:       userfolder/scripts/tcpdump_m3u8.sh 	удалено:       userfolder/scripts/winetricks.sh

---
+++ userfolder/scripts/delete_ntfs-2TB.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+
+doas chflags -R noschg /ntfs-2TB/obj/ntfs-2TB
+doas rm -r   /ntfs-2TB/obj/ntfs-2TB
--- userfolder/scripts/winetricks.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-cd /usr/local/bin
-doas fetch  https://raw.githubusercontent.com/Winetricks/winetricks/master/src/winetricks
-doas chmod +x winetricks 
