a69d662357f3cd45ef010da4a71f1554a859d983
Your Name <you@example.com>
2025-01-27 10:06:17 +0300

    изменено:      userfolder/f.sh

--- userfolder/f.sh
+++ userfolder/f.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
-
 login=$1
+
+
+
+if [ -z "$login" ]
+then
+    echo "введите логин" 
+    exit
+else
+     echo "\$var не пустая"
+fi
+
+
 rm -R ~/.mozilla/firefox
 #sshpass -p  639639 scp -r  pi@192.168.8.45:/home/pi/tinder/3/firefox ~/.mozilla
 #sshpass -p  639639 scp -r  pi@192.168.8.45:/home/pi/TelegramDesktop ~/.local/share
