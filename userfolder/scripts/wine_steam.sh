e6080cb0db85de9cf7a49de61286aedf60213418
Your Name <you@example.com>
2025-01-27 12:33:40 +0300

    изменено:      userfolder/scripts/wine_steam.sh

--- userfolder/scripts/wine_steam.sh
+++ userfolder/scripts/wine_steam.sh
@@ -16,4 +16,4 @@ fi
 
 export VK_ICD_FILENAMES=/usr/local/share/vulkan/icd.d/nvidia_icd.json
 export WINEPREFIX=/ntfs-2TB/wine
-proton "/ntfs-2TB/wine/drive_c/Program Files (x86)/Steam/steam.exe" -no-browser
+proton "/media/Steam/steam.exe" -no-browser
