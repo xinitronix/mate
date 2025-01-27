75d3897b6ff691560e9ce7b0e29b441006632ca6
Your Name <you@example.com>
2025-01-27 13:09:58 +0300

    изменено:      deinstall/del.sh

--- deinstall/del.sh
+++ deinstall/del.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-
+login=$(cat  ../accounts/user | awk '{print $1}' |  head -n1)
 if [ $(id -u) -ne 0 ]
   then echo Please run this script as root or using sudo!
   exit
@@ -12,6 +12,7 @@ rm   /boot/defaults/loader.conf
 rm   /boot/loader.conf
 rm   /etc/rc.conf
 rm   /etc/fstab
+rm   -r  /var/run/xdg/$login
 cp   rc.conf /etc/rc.conf
 cp   loader.conf /boot/loader.conf 
 cp   defaults/loader.conf   /boot/defaults/loader.conf
