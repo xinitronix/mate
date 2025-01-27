11a3a1eb4388b4890b6d8a01fc77d4fb988151b5
Your Name <you@example.com>
2025-01-27 06:18:32 +0300

    изменено:      accounts/create_user.sh 	удалено:       userfolder/.Xauthority 	удалено:       userfolder/.livestreamerrc 	удалено:       userfolder/.megarc

--- accounts/create_user.sh
+++ accounts/create_user.sh
@@ -3,11 +3,6 @@
 cat  user  | while read line
    do
       
-login=$(echo $line | awk '{print $1}' )
-passwd=$(echo $line | awk '{print $2}')
-echo $passwd | pw useradd $login -m -g wheel -s /bin/csh -h 0
-              pw groupmod video    -m $login
-              pw groupmod operator -m $login
-              pw groupmod vboxusers -m $login
+echo $line
 
    done
