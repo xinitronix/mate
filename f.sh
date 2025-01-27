5ba3119ec58c00d786c4855c3081d4ebfa81583b
Your Name <you@example.com>
2025-01-27 14:01:00 +0300

    изменено:      f.sh

--- f.sh
+++ f.sh
@@ -10,7 +10,7 @@ else
 fi
 
 
-rm -R /home/$login/.mozilla/firefox
+rm -R /home/$login/.mozilla/
 #sshpass -p  639639 scp -r  pi@192.168.8.45:/home/pi/tinder/3/firefox ~/.mozilla
 #sshpass -p  639639 scp -r  pi@192.168.8.45:/home/pi/TelegramDesktop ~/.local/share
 #fetch https://addons.mozilla.org/firefox/downloads/file/3816867/ublock_origin-1.37.2-an+fx.xpi
