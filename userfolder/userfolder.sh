636bf87667526eb472bef4cdcb8796fdb4a56ae8
Your Name <you@example.com>
2025-01-28 19:47:11 +0300

    изменено:      userfolder/userfolder.sh

--- userfolder/userfolder.sh
+++ userfolder/userfolder.sh
@@ -103,10 +103,7 @@ fi
 
 
 rm -R /home/$login/.mozilla/
-#sshpass -p  639639 scp -r  pi@192.168.8.45:/home/pi/tinder/3/firefox ~/.mozilla
-#sshpass -p  639639 scp -r  pi@192.168.8.45:/home/pi/TelegramDesktop ~/.local/share
-#fetch https://addons.mozilla.org/firefox/downloads/file/3816867/ublock_origin-1.37.2-an+fx.xpi
-#firefox ublock_origin-1.37.2-an+fx.xpi
+
  zpool import zada2
  zpool import media1
  zpool import media2
