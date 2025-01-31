2e76a8ac01009882f232a91554a77f8b0ff01aa5
Your Name <you@example.com>
2025-01-31 09:20:16 +0300

    изменено:      proxy/install.sh 	изменено:      proxy/opera-proxy/opera-proxy.desktop

--- proxy/install.sh
+++ proxy/install.sh
@@ -4,6 +4,6 @@ cp hola-proxy/hola-proxy.svg     /usr/local/share/pixmaps
 cp hola-proxy/hola-proxy.py      /usr/local/bin
 cp hola-proxy/hola-proxy.desktop /usr/local/share/applications
 
-cp hola-proxy/opera-proxy.jpg    /usr/local/share/pixmaps
-cp hola-proxy/opera-proxy.py      /usr/local/bin
-cp hola-proxy/opera-proxy.desktop /usr/local/share/applications
+cp opera-proxy/opera-proxy.jpg    /usr/local/share/pixmaps
+cp opera-proxy/opera-proxy.py      /usr/local/bin
+cp opera-proxy/opera-proxy.desktop /usr/local/share/applications
