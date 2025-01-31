46e3a4a2d2a939cf4cbb81a8a8606bd720945db9
Your Name <you@example.com>
2025-01-31 09:03:44 +0300

    изменено:      deinstall/dump 	изменено:      proxy/install.sh 	новый файл:    proxy/opera-proxy/opera-proxy.desktop 	новый файл:    proxy/opera-proxy/opera-proxy.jpg 	новый файл:    proxy/opera-proxy/opera-proxy.py

--- proxy/install.sh
+++ proxy/install.sh
@@ -3,3 +3,7 @@
 cp hola-proxy/hola-proxy.svg     /usr/local/share/pixmaps
 cp hola-proxy/hola-proxy.py      /usr/local/bin
 cp hola-proxy/hola-proxy.desktop /usr/local/share/applications
+
+cp hola-proxy/opera-proxy.jpg    /usr/local/share/pixmaps
+cp hola-proxy/opera-proxy.py      /usr/local/bin
+cp hola-proxy/opera-proxy.desktop /usr/local/share/applications
