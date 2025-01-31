0367a718a3d0ef7e4f1b4e9c11bd14775ea589c1
Your Name <you@example.com>
2025-01-31 10:46:42 +0300

    изменено:      install.sh 	новый файл:    install_item 	новый файл:    ssh/install.sh 	новый файл:    userfolder/install.sh

--- install.sh
+++ install.sh
@@ -26,100 +26,14 @@ CURRENTDIRECTORY=$(pwd)
            cd .. 
 
           ./config.sh
-          
-          cd $CURRENTDIRECTORY/dzenconky
-          ./install.sh
-          cd ..
-          cd $CURRENTDIRECTORY/etc
-          ./install.sh
-          cd .. 
-          cd $CURRENTDIRECTORY/tint2
-          ./install.sh
-          cd ..
-          cd $CURRENTDIRECTORY/switchscreen
-          ./install.sh
-          cd ..
- #         cd $CURRENTDIRECTORY/postinstall
- #         ./install.sh
- #        cd ..  
-          cd $CURRENTDIRECTORY/noteconky
-          ./install.sh
-          cd .. 
-          cd $CURRENTDIRECTORY/micro_httpd
-          ./install.sh
-          cd .. 
-          cd $CURRENTDIRECTORY/weatherconky
-          ./install.sh
-          cd ..  
-          cd $CURRENTDIRECTORY/monitoring
-          ./install.sh
-          cd ..
-         
-          cd $CURRENTDIRECTORY/worldtime
-          ./install.sh 
-          cd .. 
-          
-         
-          cd $CURRENTDIRECTORY/ubuntu_chroot
-          ./install.sh
-          cd .. 
-
-          cd $CURRENTDIRECTORY/webcamd
-          ./install.sh 
-          cd .. 
-          cd $CURRENTDIRECTORY/binance
-          ./install.sh 
-          cd .. 
-
-          cd $CURRENTDIRECTORY/wayland/dtaoconky
-          ./install.sh 
-          cd ..
- 
-          cd $CURRENTDIRECTORY/wayland/sfwbar
-          ./install.sh 
-          cd ..
-
-          
-  #        cd $CURRENTDIRECTORY/xboxdrv
-  #        ./install.sh 
-  #        cd .. 
-         
-  #        cd $CURRENTDIRECTORY/dwm
-  #        ./install.sh 
-  #        cd .. 
-  
-          cd $CURRENTDIRECTORY/power
-          ./install.sh 
-          cd .. 
-
-          cd $CURRENTDIRECTORY/zapret
-          ./install.sh 
-          cd .. 
-
-         cd $CURRENTDIRECTORY/dockerbox
-          ./install.sh 
-         cd ..
-        
-         
-        cd $CURRENTDIRECTORY/userfolder
-         ./userfolder.sh
-        cd .. 
-
-   cd $CURRENTDIRECTORY/obmenu-generator
-          ./install.sh 
-         cd ..
 
 
-        cd $CURRENTDIRECTORY/scrollconky
-          ./install.sh 
-          cd .. 
-  
-         cd $CURRENTDIRECTORY/proxy
-          ./install.sh 
-          cd ..
+       for i in $(cat install_item); do
 
-        cd $CURRENTDIRECTORY/ssh
-          ./ssh_enc.sh
-          cd .. 
+    cd $CURRENTDIRECTORY/$i
+     ./install.sh
+     cd ..
+         done
+
 
 # reboot
---
+++ userfolder/install.sh
@@ -0,0 +1,1 @@
+userfolder.sh
