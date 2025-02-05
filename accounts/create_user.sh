c32979f85984b93ee544a8cf408613e6118d7de3
Your Name <you@example.com>
2025-02-06 01:13:59 +0300

    изменено:      accounts/create_user.sh

--- accounts/create_user.sh
+++ accounts/create_user.sh
@@ -8,6 +8,5 @@ passwd=$(echo $line | awk '{print $2}')
 echo $passwd | pw useradd $login -m -g wheel -s /bin/csh -h 0
               pw groupmod video    -m $login
               pw groupmod operator -m $login
-              pw groupmod vboxusers -m $login
 
-   done
+      done
