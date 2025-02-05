eac7e0518bc53afc3a07bbfca0cd66fc73a9712f
Your Name <you@example.com>
2025-02-05 07:42:50 +0300

    изменено:      doas/install.sh

--- doas/install.sh
+++ doas/install.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 cat  ../accounts/user  | while read line
+
    do
+      login=$(echo $line | awk '{print $1}' )
       echo -e "permit nopass keepenv $login\n"            >>    /usr/local/etc/doas.conf
    done
