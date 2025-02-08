189f951bf84198d31570b64d56ec0e51aadc8fc4
Your Name <you@example.com>
2025-02-08 17:38:14 +0300

    изменено:      dzenconky/dzen2-on-off 	изменено:      obmenu-generator/install.sh

--- obmenu-generator/install.sh
+++ obmenu-generator/install.sh
@@ -1,8 +1,27 @@
 #!/bin/sh 
+
+
+
+install_first_user () {
+
 login=$(cat  ../accounts/user | awk '{print $1}' |  head -n1)
 CURRENTDIRECTORY=$(pwd)
+install_obmenu
+
+}
+ 
+install_all_user () {
+
+cat  ../accounts/user  | while read line
+
+   do
+    install_obmenu
+   done
+
+}
 
 
+install_obmenu () {
 cp             obmenu-generator    /usr/local/bin
 mkdir  -p                          /home/$login/.config/obmenu-generator
 chown  -R      $login:wheel        /home/$login/.config/obmenu-generator
@@ -10,3 +29,7 @@ cp             schema.pl           /home/$login/.config/obmenu-generator
 /usr/local/bin/cpanm Linux::DesktopFiles
 #/usr/local/bin/gsed -i -e  '1 s/^/use XSLoader;\n/' /usr/local/lib/perl5/site_perl/mach/5.32/GDBM_File.pm
 su $login -c 'obmenu-generator -p -i'
+}
+
+
+install_first_user
