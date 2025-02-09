1c0c9205bd4c01c72bdd02b3d38a0e49aa0792de
Your Name <you@example.com>
2025-02-10 01:51:31 +0300

    изменено:      accounts/create_user.sh

--- accounts/create_user.sh
+++ accounts/create_user.sh
@@ -1,12 +1,31 @@
 #!/bin/sh
 
-cat  user  | while read line
-   do
-      
+
+add_user () {      
 login=$(echo $line | awk '{print $1}' )
 passwd=$(echo $line | awk '{print $2}')
 echo $passwd | pw useradd $login -m -g wheel -s /bin/csh -h 0
               pw groupmod video    -m $login
               pw groupmod operator -m $login
+}
 
-      done
+
+cat  user  | while read line
+  
+
+do
+
+if [ -z "$line" ]
+then
+     echo "\$var Пустая"
+else
+     echo "\$var не пустая"
+ 
+     add_user 
+     
+
+fi
+
+done
+
+
