03ad91703c79bab5f58e7212b4df24a395f29e39
Your Name <you@example.com>
2025-02-09 23:02:41 +0300

    изменено:      dzenconky/install.sh

--- dzenconky/install.sh
+++ dzenconky/install.sh
@@ -1,9 +1,27 @@
 #!/bin/sh
 
-login=$(cat  ../accounts/user | awk '{print $1}' |  head -n1)
 CURRENTDIRECTORY=$(pwd)
 
+install_first_user () {
+login=$(cat  ../accounts/user | awk '{print $1}' |  head -n1)
+install_dzen2
 
+}
+
+
+install_all_user () {
+
+cat  ../accounts/user  | while read line
+
+   do
+    login=$(echo $line | awk '{print $1}' )
+    echo $login
+    install_dzen2
+   done
+
+}
+
+install_dzen2 () {
 
 cp $CURRENTDIRECTORY/switchbackground      /usr/local/bin
 cp $CURRENTDIRECTORY/dzenconky             /usr/local/bin
@@ -19,4 +37,7 @@ cp $CURRENTDIRECTORY/dzen2-on-off          /usr/local/bin
                       echo   '"/usr/local/bin/dzen2-on-off"'             >> /home/$login/.xbindkeysrc
                       echo   'm:0x40 + c:40'                             >> /home/$login/.xbindkeysrc
                       echo   'Mod4 + d'                                  >> /home/$login/.xbindkeysrc
-        
+   }
+
+
+install_all_user 
