84cecdfdfacbeb8580c0a8f58acc841a0614d1be
Your Name <you@example.com>
2025-02-11 10:09:35 +0300

    изменено:      obmenu-generator/install.sh

--- obmenu-generator/install.sh
+++ obmenu-generator/install.sh
@@ -40,7 +40,7 @@ chown  -R      $login:wheel        /home/$login/.config/obmenu-generator
 cp             schema.pl           /home/$login/.config/obmenu-generator
 /usr/local/bin/cpanm Linux::DesktopFiles
 #/usr/local/bin/gsed -i -e  '1 s/^/use XSLoader;\n/' /usr/local/lib/perl5/site_perl/mach/5.32/GDBM_File.pm
-su $login -c 'obmenu-generator -p -i'
+su $login -c '/usr/local/bin/obmenu-generator -p -i'
 }
 
 
