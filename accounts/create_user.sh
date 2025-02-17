1931f7ad34fb498299b3b2b7bb60ae09542d6adc
Your Name <you@example.com>
2025-02-17 15:31:22 +0300

    Изменения, которые будут включены в коммит:

--- accounts/create_user.sh
+++ accounts/create_user.sh
@@ -5,9 +5,13 @@ dir=$(dirname "$(realpath $0)")
 add_user () {      
 login=$(echo $line | awk '{print $1}' )
 passwd=$(echo $line | awk '{print $2}')
+
+if ! getent passwd $login  > /dev/null ; then
+echo "пользователь $login  не существует"
 echo $passwd | pw useradd $login -m -g wheel -s /bin/csh -h 0
               pw groupmod video    -m $login
               pw groupmod operator -m $login
+      fi
 }
 
 cat  $dir/user  | while read line
