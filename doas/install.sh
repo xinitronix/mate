c3b8f133c3790acbae24a04b566bdf504210f9ae
Your Name <you@example.com>
2025-02-18 08:30:12 +0300

    изменено:      doas/install.sh

--- doas/install.sh
+++ doas/install.sh
@@ -1,6 +1,18 @@
 #!/bin/sh
 
+if test "$(id -u)" -ne 0; then
+	printf "%s must be run as root\n" "${0##*/}"
+	exit 1
+fi
+
 dir=$(dirname "$(realpath $0)")
+
+if [  -f "/usr/local/etc/mate.pid/doas_install.pid" ]; then
+    echo "Файл  doas_install.pid  существует"
+    echo "Пропускаем установку doas"
+    exit 
+fi
+
 
 add () {
       login=$(echo $line | awk '{print $1}' )
@@ -21,3 +33,6 @@ fi
 
  /usr/local/bin/gsed   -i '/^[[:space:]]*$/d' /usr/local/etc/doas.conf
 # /usr/local/bin/gsed   -i '$d'                /usr/local/etc/doas.conf
+
+mkdir -p /usr/local/etc/mate.pid
+touch /usr/local/etc/mate.pid/doas_install.pid
