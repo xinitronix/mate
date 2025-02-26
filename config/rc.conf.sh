e9a5ef9e1f04fe2f03ab94371cbe68ceebf4b4e3
Your Name <you@example.com>
2025-02-26 18:43:44 +0300

    изменено:      config/rc.conf.sh 	изменено:      micro_httpd/install.sh

--- config/rc.conf.sh
+++ config/rc.conf.sh
@@ -32,3 +32,4 @@ fi
                      echo 'ifconfig_re0="inet 192.168.8.101 netmask 255.255.255.0"'   >>           /etc/rc.conf
                      echo 'defaultrouter="192.168.8.1"'                               >>           /etc/rc.conf
                      echo 'xboxdrv_enable="YES"'                                      >>           /etc/rc.conf 
+                     echo 'inetd_enable=YES'                                          >>           /etc/rc.conf
