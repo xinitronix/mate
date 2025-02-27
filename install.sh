2e054e1337e6d670b667ae56b9641c5d9390ec48
Your Name <you@example.com>
2025-02-27 23:49:52 +0300

    изменено:      install.sh

--- install.sh
+++ install.sh
@@ -9,10 +9,10 @@ mkdir -p /usr/local/bin
 export PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin
 pkg fetch -y -r myrepo -o . indexinfo
 pkg fetch -y -r myrepo -o . desktop-file-utils
-tar xf ./desktop-file-utils-0.28.pkg  -s ",/.*/,,g" "*/update-desktop-database"
-tar xf ./indexinfo-0.3.1.pkg -s ",/.*/,,g" "*/indexinfo"
-cp  indexinfo /usr/local/bin
-cp  update-desktop-database  /usr/local/bin
+tar xf $dir/desktop-file-utils-0.28.pkg  -s ",/.*/,,g" "*/update-desktop-database"
+tar xf $dir/indexinfo-0.3.1.pkg -s ",/.*/,,g" "*/indexinfo"
+cp  $dir/indexinfo /usr/local/bin
+cp  $dir/update-desktop-database  /usr/local/bin
 
 if [ -z "$1" ]
 then
