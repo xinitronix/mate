fc8352f705c2bb75d4529f0d482f5252204ed3f4
Your Name <you@example.com>
2025-03-02 22:37:05 +0300

    изменено:      userfolder/openbox.autostart.sh

--- userfolder/openbox.autostart.sh
+++ userfolder/openbox.autostart.sh
@@ -6,6 +6,6 @@ if [ -f "$file" ] ; then
 fi
  echo 'feh --bg-scale /home/definitly/2TB/wallpaper/wallhaven-q6gj8d.jpg &' >> autostart.sh
  echo 'xxkb &' >> autostart.sh
- echo 'firefox-dev &' >> autostart.sh
+ echo 'firefox &' >> autostart.sh
  echo '#~/scripts/x2.sh &' >> autostart.sh
  echo 'alacritty -e screen_check.sh &' >> autostart.sh
