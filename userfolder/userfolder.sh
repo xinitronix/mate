028532e02be5637e5d483e0e8c9e965e6771b1e9
Your Name <you@example.com>
2025-03-26 10:17:53 +0300

    изменено:      userfolder/userfolder.sh

--- userfolder/userfolder.sh
+++ userfolder/userfolder.sh
@@ -98,7 +98,7 @@ cp  -R  /tmp/userfolder/scripts                   /home/$login/
 #update-desktop-database
 cp    -R  /tmp/userfolder/.local/share/applications    /home/$login/.local/share/
 #Proton Experimental 
-cp    ../etc/proton.desktop              /home/$login/.local/share/applications 
+cp    ../etc/proton.desktop                            /home/$login/.local/share/applications 
 /usr/local/bin/update-desktop-database                 /home/$login/.local/share/applications
  
 su $login -c 'xdg-mime default echoplaylist.firefox.desktop x-scheme-handler/echo'
