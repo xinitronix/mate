dfeba28aa58490c8bfda32f5fae4592998a1880a
Your Name <you@example.com>
2025-03-28 16:37:56 +0300

    изменено:      userfolder/userfolder.sh

--- userfolder/userfolder.sh
+++ userfolder/userfolder.sh
@@ -141,7 +141,11 @@ cp  ../etc/rootpcmanfm.desktop                   /usr/local/share/applications
 cp ../etc/icons/system-file-manager.svg /usr/local/share/icons/hicolor/scalable/apps
 
 ln -s /ntfs-2TB/i386-wine-pkg  /home/$login/.i386-wine-pkg
- 
+
+#steam-bottler
+
+ln -s /ntfs-2TB/steam-bottler   /home/$login/steam-bottler
+
 cp -R "/ntfs-2TB/freebsd config/firefox"          /home/$login/.mozilla
 cp -R "/ntfs-2TB/freebsd config/TelegramDesktop"  /home/$login/.local/share
 cp -R "/ntfs-2TB/freebsd config/google-chrome"    /home/$login/.config
