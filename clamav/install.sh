53920f85f7c225f4df6a0f4cc232ad2300e81e6a
Your Name <you@example.com>
2025-04-08 20:49:03 +0300

    новый файл:    clamav/clamav-gui/clamd.conf 	новый файл:    clamav/clamav-gui/clamd.log 	новый файл:    clamav/clamav-gui/freshclam.conf 	новый файл:    clamav/clamav-gui/freshclam.log 	новый файл:    clamav/clamav-gui/logs/report-scan.log 	новый файл:    clamav/clamav-gui/profiles/.ini 	новый файл:    clamav/clamav-gui/settings.ini 	новый файл:    clamav/clamav-gui/update.log 	изменено:      clamav/install.sh

--- clamav/install.sh
+++ clamav/install.sh
@@ -6,8 +6,38 @@ if [  -f "/usr/local/etc/mate.pid/clamav_install.pid" ]; then
     exit 
 fi
 
+install_first_user () {
+
+login=$(cat  $dir/../accounts/user | awk '{print $1}' |  head -n1)
+CURRENTDIRECTORY=$(pwd)
+install_clamav
+
+}
+
+install_all_user () {
+
+cat  $dir/../accounts/user  | while read line
+ do
+   if [ -z "$line" ]
+then
+     echo "\$var Пустая"
+ else
+     echo "\$var не пустая"
+ 
+    login=$(echo $line | awk '{print $1}' )
+    echo $login
+    install_clamav
+fi
+  done
+}
+
+install_clamav () {
 dir=$(dirname "$(realpath $0)")
 cp  $dir/freshclam.conf  /usr/local/etc
+cp -R $dir/clamav-gui    /home/$login/.config
+}
+
+install_all_user
 
 mkdir -p /usr/local/etc/mate.pid
 touch /usr/local/etc/mate.pid/clamav_install.pid
