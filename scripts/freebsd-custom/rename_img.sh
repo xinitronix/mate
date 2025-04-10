f4387dba75c31159d5f1d6e62c1263313cc0f257
Your Name <you@example.com>
2025-04-10 16:46:36 +0300

    новый файл:    scripts/freebsd-custom/rename_img.sh

---
+++ scripts/freebsd-custom/rename_img.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+img=$(uname -s)-$(uname -r)-$(uname -m)-$(date '+%Y%m%d')-$(cd /usr/src && git rev-parse HEAD | cut -c1-12)-memstick.img
+cp /usr/obj/ntfs-2TB/src/amd64.amd64/release/memstick.img   $HOME/$img
