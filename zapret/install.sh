2a25e5b330dfb07a965b1f794f71a41bf0cff072
Your Name <you@example.com>
2025-05-07 06:48:54 +0300

    изменено:      zapret/install.sh

--- zapret/install.sh
+++ zapret/install.sh
@@ -13,7 +13,7 @@ echo 'firewall_script="/etc/rc.firewall.my"' >> /etc/rc.conf
 echo 'ipfw -q -f flush' >> /etc/rc.firewall.my
 echo 'ipfw add 100 divert 989 tcp from any to any 80,443 out not diverted xmit re0' >> /etc/rc.firewall.my
 echo 'ipfw add 100 divert 989 tcp from any 80,443 to any tcpflags syn,ack in not diverted recv re0' >> /etc/rc.firewall.my
-echo '/usr/local/bin/dvtws --port 989 --dpi-desync=fake --dpi-desync-repeats=6 --dpi-desync-ttl=0 --dpi-desync-any-protocol --dpi-desync-cutoff=d4 --dpi-desync-fooling=md5sig,badsum &' >> /etc/rc.firewall.my
+echo '/usr/local/bin/dvtws --port 989 --dpi-desync=fake --dpi-desync-repeats=6 --dpi-desync-ttl=0 --dpi-desync-any-protocol --dpi-desync-cutoff=d4 --dpi-desync-fooling=badseq,badsum &' >> /etc/rc.firewall.my
 
 mkdir -p /usr/local/etc/mate.pid
 touch /usr/local/etc/mate.pid/zapret_install.pid
