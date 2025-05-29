c3a0c6ee6a39e9c413ea05e01e13fc1fec421176
Your Name <you@example.com>
2025-05-29 20:37:21 +0300

    изменено:      deinstall/dump 	изменено:      install_item 	новый файл:    linuxbox/dockerbox.desktop 	новый файл:    linuxbox/dockerbox.py 	новый файл:    linuxbox/etc/linuxbox.conf 	новый файл:    linuxbox/install.sh 	новый файл:    linuxbox/rc.d/linuxbox 	новый файл:    linuxbox/sbin/linuxbox

---
+++ linuxbox/install.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+cp rc.d/linuxbox      /etc/rc.d
+cp sbin/linuxbox      /usr/local/sbin
+#cp dockerbox.py        /usr/local/bin
+mkdir -p               /usr/local/etc/linuxbox
+cp etc/linuxbox.conf  /usr/local/etc/linuxbox
+#cp dockerbox.desktop   /usr/local/share/applications
