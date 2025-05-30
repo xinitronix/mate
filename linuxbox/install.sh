17ecd3dbb0ed9ce3c8f19249e8ac8cb985d08107
Your Name <you@example.com>
2025-05-30 18:24:44 +0300

    изменено:      deinstall/dump 	изменено:      linuxbox/install.sh 	новый файл:    linuxbox/switch_vm/switch_vm_off 	новый файл:    linuxbox/switch_vm/switch_vm_on

--- linuxbox/install.sh
+++ linuxbox/install.sh
@@ -5,3 +5,5 @@ cp sbin/linuxbox      /usr/local/sbin
 mkdir -p               /usr/local/etc/linuxbox
 cp etc/linuxbox.conf  /usr/local/etc/linuxbox
 #cp dockerbox.desktop   /usr/local/share/applications
+cp  switch_vm/switch_vm_off  /usr/local/bin
+cp  switch_vm/switch_vm_on   /usr/local/bin
