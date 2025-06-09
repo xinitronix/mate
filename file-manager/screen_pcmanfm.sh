7a0cc341669cca0cc22ef34bd25a0ee85e244793
Your Name <you@example.com>
2025-06-09 14:37:20 +0300

    изменено:      file-manager/screen_pcmanfm.sh

--- file-manager/screen_pcmanfm.sh
+++ file-manager/screen_pcmanfm.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
-#SHELL_CWD=$(pwd -P)
-#alacritty --working-directory  $SHELL_CWD
-
 dir=$(pwd -P)
-
 var=$(screen -r | awk '(NR == 2)' | awk '{print $1}')
 
 echo $dir
-screen -d -r  $var -X stuff "cd $dir ^M"
+screen -d -r  $var -X stuff "cd $dir ^M screen -t pcmanfm ^M"
