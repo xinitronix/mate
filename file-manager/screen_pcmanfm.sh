b8c2c49b12b161d611f42be2dc4c21ec232e627e
Your Name <you@example.com>
2025-06-11 15:06:54 +0300

    Изменения, которые будут включены в коммит: 	изменено:      file-manager/screen_pcmanfm.sh

--- file-manager/screen_pcmanfm.sh
+++ file-manager/screen_pcmanfm.sh
@@ -3,4 +3,4 @@ dir=$(pwd -P)
 var=$(screen -r | awk '(NR == 2)' | awk '{print $1}')
 
 echo $dir
-screen -d -r  $var -X stuff "cd $dir ^M screen -t pcmanfm ^M"
+screen -d -r  $var -X stuff "cd $dir ^M "
