477b10f2857cfc900a7147372ebc31b181be5cd5
Your Name <you@example.com>
2025-06-14 20:19:51 +0300

    изменено:      userfolder/zshrc.sh

--- userfolder/zshrc.sh
+++ userfolder/zshrc.sh
@@ -6,10 +6,10 @@
  echo 'HAS_WIDECHARS="false"' >> .zshrc
  echo 'export TERM=xterm-256color' >> .zshrc
  echo '# Source manjaro-zsh-configuration' >> .zshrc
- echo 'if [[ -e /usr/share/zsh/manjaro-zsh-config ]]; then' >> .zshrc
- echo 'source /usr/share/zsh/manjaro-zsh-config' >> .zshrc
+ echo 'if [[ -e /usr/local/share/zsh/manjaro-zsh-config ]]; then' >> .zshrc
+ echo 'source /usr/local/share/zsh/manjaro-zsh-config' >> .zshrc
  echo 'fi' >> .zshrc
  echo '# Use manjaro zsh prompt' >> .zshrc
- echo 'if [[ -e /usr/share/zsh/manjaro-zsh-prompt ]]; then' >> .zshrc
- echo 'source /usr/share/zsh/manjaro-zsh-prompt' >> .zshrc
+ echo 'if [[ -e /usr/local/share/zsh/manjaro-zsh-prompt ]]; then' >> .zshrc
+ echo 'source /usr/local/share/zsh/manjaro-zsh-prompt' >> .zshrc
  echo 'fi' >> .zshrc
