9c5a9ee99ca4a7b2ed42ee5cc2ee17a573f228d0
Your Name <you@example.com>
2025-07-02 16:52:36 +0300

    изменено:      install_item 	удалено:       linuxbox/dockerbox.desktop 	удалено:       linuxbox/dockerbox.py 	изменено:      linuxbox/install.sh 	новый файл:    winbox/etc/loader.conf.linuxbox 	новый файл:    winbox/etc/loader.conf.winbox 	новый файл:    winbox/etc/winbox.conf 	новый файл:    winbox/install.sh 	новый файл:    winbox/rc.d/winbox 	новый файл:    winbox/sbin/winbox 	новый файл:    winbox/switch_vm/switch_winvm_off 	новый файл:    winbox/switch_vm/switch_winvm_on

---
+++ winbox/install.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+cp rc.d/winbox      /etc/rc.d
+cp sbin/winbox      /usr/local/sbin
+mkdir -p               /usr/local/etc/winbox
+cp etc/winbox.conf  /usr/local/etc/winbox
+cp etc/loader.conf.winbox  /usr/local/etc/winbox
+cp etc/loader.conf.linuxbox  /usr/local/etc/winbox
+cp  switch_vm/switch_winvm_off  /usr/local/bin
+cp  switch_vm/switch_winvm_on   /usr/local/bin
