903bbb19c73c46f8b12e2814de30a42182bff0ab
Your Name <you@example.com>
2025-09-06 17:13:55 +0300

    изменено:      etc/myrepo 	изменено:      scripts/install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -26,7 +26,7 @@ pkg install  -r myrepo  -y  $i
 done
 }
 
-if [ "14.3-STABLE" = "$(uname -r)"  ] || [ "15.0-PRERELEASE" = "$(uname -r)"  ] ; then
+if [ "14.3-STABLE" = "$(uname -r)"  ] || [ "14.0" = "$(uname -r)"  ] ; then
     install_14
     echo "install packages freebsd 14"
 else
