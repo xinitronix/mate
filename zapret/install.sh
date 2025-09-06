e39c998231900345d8ad4362628b14c4edb0318b
Your Name <you@example.com>
2025-09-06 17:31:40 +0300

    изменено:      zapret/install.sh 	переименовано: zapret/zapret.config -> zapret/zapret.config.FreeBSD14 	новый файл:    zapret/zapret.config.FreeBSD15

--- zapret/install.sh
+++ zapret/install.sh
@@ -15,6 +15,17 @@ echo 'ipfw -q -f flush' >> /etc/rc.firewall.my
 echo 'ipfw add 100 divert 989 tcp from any to any 80,443 out not diverted xmit re0' >> /etc/rc.firewall.my
 echo 'ipfw add 100 divert 989 tcp from any 80,443 to any tcpflags syn,ack in not diverted recv re0' >> /etc/rc.firewall.my
 echo '/usr/local/bin/dvtws --port 989 $CONFIG &' >> /etc/rc.firewall.my
+
+uname=$(uname -r | grep 15)
+
+if [ -z "$line" ]
+then
+     echo "\$var Пустая"
+  cp  zapret.config.FreeBSD14 /etc/zapret.config/zapret.config
+else
+     cp  zapret.config.FreeBSD15 /etc/zapret.config
+fi
+
 cp  zapret.config /etc 
 mkdir -p /usr/local/etc/mate.pid
 touch /usr/local/etc/mate.pid/zapret_install.pid
