06921227a5c1f75a145dc50c3e9991c2832954e6
Your Name <you@example.com>
2025-09-08 18:27:30 +0300

    новый файл:    i915/20-intel.conf 	новый файл:    i915/change_rc.conf.sh 	новый файл:    i915/install.sh 	новый файл:    i915/xorg.conf

---
+++ i915/change_rc.conf.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+if test "$(id -u)" -ne 0; then
+	printf "%s must be run as root\n" "${0##*/}"
+	exit 1
+fi
+
+set_i915kms () {
+               gsed -i  '\|^kld_list|d'  /etc/rc.conf
+               echo 'kld_list="linux64 nullfs cuse i915kms"' >> /etc/rc.conf
+               rm /etc/X11/xorg.conf
+               cp 20-intel.conf  /usr/local/etc/X11/xorg.conf.d/
+}
+
+set_nvidia () {
+               gsed -i  '\|^kld_list|d'  /etc/rc.conf
+               echo 'kld_list="linux64 nullfs cuse nvidia-drm"' >> /etc/rc.conf
+               rm   /usr/local/etc/X11/xorg.conf.d/20-intel.conf
+               cp   xorg.conf   /etc 	
+}
+
+line=$(cat /etc/rc.conf | grep i915kms)
+
+if [ -z "$line" ]
+then
+     echo "\$var Пустая set mode i915"
+     set_i915kms
+else
+     echo "\$var не пустая set mode nvidia"
+     set_nvidia 
+fi
---
+++ i915/install.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+cp 20-intel.conf  /usr/local/etc/X11/xorg.conf.d/
