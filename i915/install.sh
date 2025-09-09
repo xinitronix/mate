a456a83cb943fddcd75c57f468a0db551f796c6d
Your Name <you@example.com>
2025-09-09 11:29:01 +0300

    изменено:      i915/install.sh 	изменено:      i915/switch_i915.sh

--- i915/install.sh
+++ i915/install.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
 cp switch_i915.sh /usr/local/bin/switch_i915
+mkdir -p /usr/local/etc/i915
+cp  20-intel.conf  /usr/local/etc/i915
+cp xorg.conf      /usr/local/etc/i915
--- i915/switch_i915.sh
+++ i915/switch_i915.sh
@@ -9,14 +9,14 @@ set_i915kms () {
                gsed -i  '\|^kld_list|d'  /etc/rc.conf
                echo 'kld_list="linux64 nullfs cuse i915kms"' >> /etc/rc.conf
                rm /etc/X11/xorg.conf
-               cp 20-intel.conf  /usr/local/etc/X11/xorg.conf.d/
+               cp  /usr/local/etc/20-intel.conf  /usr/local/etc/X11/xorg.conf.d/
 }
 
 set_nvidia () {
                gsed -i  '\|^kld_list|d'  /etc/rc.conf
                echo 'kld_list="linux64 nullfs cuse nvidia-drm"' >> /etc/rc.conf
                rm   /usr/local/etc/X11/xorg.conf.d/20-intel.conf
-               cp   xorg.conf   /etc 	
+               cp   /usr/local/etc/xorg.conf   /etc/X11	
 }
 
 line=$(cat /etc/rc.conf | grep i915kms)
