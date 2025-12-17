7cf9e7f601da0674684f9d6ace418ce253698813
Your Name <you@example.com>
2025-12-17 21:37:02 +0300

    изменено:      etc/myrepo 	изменено:      scripts/install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -8,30 +8,30 @@ if [  -f "/usr/local/etc/mate.pid/pkg_install.pid" ]; then
     exit 
 fi
 
+install_16 () {
+
+for i in $(cat $dir/pkg_leaves); do
+
+pkg install  -r myrepo16 -y  $i 
+
+done
+}
+
 install_15 () {
 
 for i in $(cat $dir/pkg_leaves); do
 
-pkg install  -r myrepo15 -y  $i 
+pkg install  -r myrepo15  -y  $i 
 
 done
 }
 
-install_14 () {
-
-for i in $(cat $dir/pkg_leaves); do
-
-pkg install  -r myrepo  -y  $i 
-
-done
-}
-
-if [ "14.3-STABLE" = "$(uname -r)"  ] || [ "14.3-RELEASE" = "$(uname -r)"  ] ; then
-    install_14
-    echo "install packages freebsd 14"
-else
+if [ "15.0-STABLE" = "$(uname -r)"  ] || [ "15.0-RELEASE" = "$(uname -r)"  ] ; then
     install_15
     echo "install packages freebsd 15"
+else
+    install_16
+    echo "install packages freebsd 16"
 fi
 
 mkdir -p /usr/local/etc/mate.pid
