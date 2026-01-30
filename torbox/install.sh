922a5f6b0c51a853587641c8eebc1bf78a45bbdc
Your Name <you@example.com>
2026-01-30 08:52:10 +0300

    новый файл:    torbox/etc/torbox.conf 	новый файл:    torbox/install.sh 	новый файл:    torbox/rc.d/torbox 	новый файл:    torbox/sbin/torbox 	новый файл:    torbox/tor.png 	новый файл:    torbox/torbox.desktop 	новый файл:    torbox/torbox.py

---
+++ torbox/install.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+cp rc.d/torbox      /etc/rc.d
+cp sbin/torbox      /usr/local/sbin
+cp torbox.py        /usr/local/bin
+mkdir -p               /usr/local/etc/torbox
+cp etc/torbox.conf  /usr/local/etc/torbox
+cp torbox.desktop   /usr/local/share/applications
