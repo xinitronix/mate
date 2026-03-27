426fb9b5f44fc52ee6d2f634b778b1282885ba21
Your Name <you@example.com>
2026-03-27 05:21:20 +0300

    изменено:      install_item 	новый файл:    jwm/install.sh 	изменено:      jwm/jwmrc 	изменено:      scripts/pkg_leaves

---
+++ jwm/install.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+login=$(cat  ../accounts/user | awk '{print $1}' |  head -n1)
+CURRENTDIRECTORY=$(pwd)
+
+
+cp $CURRENTDIRECTORY/jwmrc       /home/$login/.jwmrc
+chown  -R      $login:wheel      /home/$login/.jwmrc
+
+cp $CURRENTDIRECTORY/jwmrc-mjwm      /home/$login/.jwmrc-mjwm
+chown  -R      $login:wheel      /home/$login/.jwmrc-mjwm
