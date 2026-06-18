cdf1a81c301e6daad6811833ab2c3664405989ad
Your Name <you@example.com>
2026-06-18 17:35:31 +0300

    изменено:      etc/myrepo 	изменено:      scripts/install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -26,7 +26,7 @@ pkg install  -r myrepo15  -y  $i
 done
 }
 
-if [ "15.1-STABLE" = "$(uname -r)"  ] || [ "15.1-BETA2" = "$(uname -r)"  ] ; then
+if [ "15.1-STABLE" = "$(uname -r)"  ] || [ "15.1-RELEASE" = "$(uname -r)"  ] ; then
     install_15
     echo "install packages freebsd 15"
 else
