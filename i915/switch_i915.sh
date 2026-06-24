738c8797a03190fc7052bcf1d932a1c40d6eff8c
Your Name <you@example.com>
2026-06-24 11:10:52 +0300

    изменено:      i915/switch_i915.sh

--- i915/switch_i915.sh
+++ i915/switch_i915.sh
@@ -5,6 +5,35 @@ if test "$(id -u)" -ne 0; then
 	exit 1
 fi
 
+
+RESULT=$(pciconf -l | grep "vgapci0@pci0:0:2:0")
+
+if [ -z "$RESULT" ]; then
+    zenity --error \
+    --title="Ошибка GPU" \
+    --text="Видеокарта не найдена.\nПроверь BIOS/UEFI и включи графику."
+    exit 1
+else
+    zenity --info \
+    --title="GPU OK" \
+    --text="Видеокарта обнаружена:\n$RESULT"
+    line=$(cat /etc/rc.conf | grep i915kms)
+
+if [ -z "$line" ]
+then
+     echo "\$var Пустая set mode i915"
+     set_i915kms
+else
+     echo "\$var не пустая set mode nvidia"
+     set_nvidia 
+fi
+
+
+fi
+
+
+
+
 set_i915kms () {
                gsed -i  '\|^kld_list|d'  /etc/rc.conf
                echo 'kld_list="linux64 nullfs cuse i915kms"' >> /etc/rc.conf
