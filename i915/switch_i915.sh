3c77cfdff2f15462ad090a1309ca8dbc1f5b6f1f
Your Name <you@example.com>
2026-06-29 18:41:42 +0300

    изменено:      i915/switch_i915.sh

--- i915/switch_i915.sh
+++ i915/switch_i915.sh
@@ -1,44 +1,54 @@
 #!/bin/sh
 
-if test "$(id -u)" -ne 0; then
-	printf "%s must be run as root\n" "${0##*/}"
-	exit 1
+# 1. Проверка прав root
+if [ "$(id -u)" -ne 0 ]; then
+    printf "%s: must be run as root\n" "${0##*/}"
+    exit 1
 fi
 
-RESULT=$(pciconf -l | grep "pci0:0:2:0")
+# 2. Поиск встроенной графики Intel
+RESULT=$(pciconf -l | grep -E "pci0:[0-9]+:2:0")
 
 set_i915kms () {
-               gsed -i  '\|^kld_list|d'  /etc/rc.conf
-               echo 'kld_list="linux64 nullfs cuse i915kms"' >> /etc/rc.conf
-               rm /etc/X11/xorg.conf
-               cp  /usr/local/etc/i915/20-intel.conf  /usr/local/etc/X11/xorg.conf.d/
+    echo "Переключаем режим на: Intel (i915kms)"
+    # Удаляем старый kld_list и записываем новый
+    sed -i '' '/^kld_list=/d' /etc/rc.conf
+    echo 'kld_list="linux64 nullfs cuse i915kms"' >> /etc/rc.conf
+    
+    # Настройка Xorg для Intel
+    rm -f /etc/X11/xorg.conf
+    mkdir -p /usr/local/etc/X11/xorg.conf.d
+    cp /usr/local/etc/i915/20-intel.conf /usr/local/etc/X11/xorg.conf.d/
 }
 
 set_nvidia () {
-               gsed -i  '\|^kld_list|d'  /etc/rc.conf
-               echo 'kld_list="linux64 nullfs cuse nvidia-drm"' >> /etc/rc.conf
-               rm   /usr/local/etc/X11/xorg.conf.d/20-intel.conf
-               cp   /usr/local/etc/i915/xorg.conf   /etc/X11	
+    echo "Переключаем режим на: NVIDIA"
+    # Удаляем старый kld_list и записываем новый
+    sed -i '' '/^kld_list=/d' /etc/rc.conf
+    echo 'kld_list="linux64 nullfs cuse nvidia-drm"' >> /etc/rc.conf
+    
+    # Настройка Xorg для NVIDIA
+    rm -f /usr/local/etc/X11/xorg.conf.d/20-intel.conf
+    cp /usr/local/etc/i915/xorg.conf /etc/X11
 }
 
+# 3. Логика проверки и переключения
 if [ -z "$RESULT" ]; then
     zenity --error \
-    --title="Ошибка GPU" \
-    --text="Видеокарта не найдена.\nПроверь BIOS/UEFI и включи графику."
+        --title="Ошибка GPU" \
+        --text="Интегрированная видеокарта Intel не найдена.\nПроверь BIOS/UEFI."
     exit 1
 else
     zenity --info \
-    --title="GPU OK" \
-    --text="Видеокарта обнаружена:\n$RESULT"
-    line=$(cat /etc/rc.conf | grep i915kms)
+        --title="GPU OK" \
+        --text="Видеокарта обнаружена:\n$RESULT"
 
-if [ -z "$line" ]
-then
-     echo "\$var Пустая set mode i915"
-     set_i915kms
-else
-     echo "\$var не пустая set mode nvidia"
-     set_nvidia 
-fi
-
+    # Проверяем, установлен (прописан) ли i915kms в rc.conf
+    if grep -q "i915kms" /etc/rc.conf; then
+        echo "Драйвер i915kms найден в rc.conf. Меняем на NVIDIA..."
+        set_nvidia
+    else
+        echo "Драйвер i915kms НЕ найден в rc.conf. Включаем Intel..."
+        set_i915kms
+    fi
 fi
