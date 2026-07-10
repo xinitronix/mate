808a9d2ac532318dd815cb6a4bad17acc6774e00
Your Name <you@example.com>
2026-07-10 12:14:45 +0300

    новый файл:    winbox/etc/ttys 	новый файл:    winbox/etc/ttys.winbox 	изменено:      winbox/install.sh 	изменено:      winbox/switch_vm/switch_winvm_off 	изменено:      winbox/switch_vm/switch_winvm_on

--- winbox/install.sh
+++ winbox/install.sh
@@ -6,5 +6,7 @@ mkdir -p               /usr/local/etc/winbox
 cp etc/winbox.conf  /usr/local/etc/winbox
 cp etc/loader.conf.winbox  /usr/local/etc/winbox
 cp etc/loader.conf.linuxbox  /usr/local/etc/winbox
+cp etc/ttys.winbox   /usr/local/etc/winbox
+cp etc/ttys   /usr/local/etc/winbox
 cp  switch_vm/switch_winvm_off  /usr/local/bin
 cp  switch_vm/switch_winvm_on   /usr/local/bin
