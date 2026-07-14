b2d6e90c1151a369223ce97fd9307a42bfc6b774
Your Name <you@example.com>
2026-07-14 03:25:58 +0300

    изменено:      config/rc.conf.sh

--- config/rc.conf.sh
+++ config/rc.conf.sh
@@ -7,7 +7,7 @@ if [ -f "$file" ] ; then
 fi
                      echo 'zfs_enable="yes"'                                          >>           /etc/rc.conf
                      echo 'dbus_enable="yes"'                                         >>           /etc/rc.conf
-                     echo 'background_dhclient=YES'                                   >>           /etc/rc.conf
+                     echo 'background_dhclient="YES"'                                   >>           /etc/rc.conf
                      echo 'linux_enable="YES"'                                        >>           /etc/rc.conf
                      echo 'dahdi_enable="YES"'                                        >>           /etc/rc.conf
                      echo 'wlans_urtwn0="wlan0"'                                      >>           /etc/rc.conf
@@ -41,3 +41,4 @@ fi
                      echo 'tun2proxy_enable="YES"'                                    >>           /etc/rc.conf
                      echo 'static_routes="myroute"'                                   >>           /etc/rc.conf
                      echo 'route_myroute="-host 94.177.106.71 192.168.8.1"'           >>           /etc/rc.conf
+                     echo 'ubuntu_enable="NO"'                                        >>           /etc/rc.conf
