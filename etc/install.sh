3e3d91df8911eba31d095faadbf883c2190c7cf3
Your Name <you@example.com>
2026-08-12 03:19:02 +0300

    изменено:      etc/install.sh

--- etc/install.sh
+++ etc/install.sh
@@ -91,4 +91,4 @@ cp $dir/torlog /usr/local/bin/
 mkdir -p /usr/local/etc/mate.pid
 touch /usr/local/etc/mate.pid/etc_install.sh.pid
 
-echo 'daily_status_security_enable="NO"' | sudo tee /etc/periodic.conf
+echo 'daily_status_security_enable="NO"' | tee /etc/periodic.conf
